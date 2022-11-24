072b59b26b8ff415e9af79f1231915c4754d640b
ZinoTrust <zinotrust@gmail.com>
2022-11-24 16:03:41 +0100

    5. Redux Toolkit Setup

---
+++ src/redux/features/auth/authService.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const API_URL = `${BACKEND_URL}/api/users/`;
---
+++ src/redux/features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+  user: null,
+  users: [],
+  twoFactor: false,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    RESET(state) {
+      state.twoFactor = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    },
+  },
+});
+
+export const { RESET } = authSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+
+export default authSlice.reducer;
